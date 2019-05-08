9b3ab0df723ae69f00a070d94862e9cda9620939
Alejandro Rosales <alejandrorosales479@gmail.com>
2019-05-08 16:46:25 -0400

    Update MyWorld.py

--- WithoutPyGame/MyWorld.py
+++ WithoutPyGame/MyWorld.py
@@ -1,15 +1,21 @@
 import random
 import NextGen as ng
-from NextGen import Stats
+from NextGen import Stats, MassExtinction
 
-pop_n = [3333, 3333, 3333]  # Number of: Humans, Drackonians, Gritiss
+pop_n = [33333, 33333, 33333]  # Number of: Humans, Gritiss, Drackonians
 weights = [random.random() for i in range(5)]
 stats = Stats()
 stats.weights_summary(weights)
+print_every = 3
 population = ng.generate_population(pop_n)
-for generation in range(4000):
-    if generation % 10 == 0:
+mass_extinction = MassExtinction()
+generation = 0
+while True:
+    if generation % print_every == 0:
         print()
-    stats.counting(population, generation, print_at=10)
-    stats.creatures_summary(population, weights, generation, print_at=10)
+    stats.counting(population, generation, print_every=print_every)
+    stats.creatures_summary(population, weights, generation, print_every=print_every)
     population = ng.evolve(population, weights)
+    
+    population = mass_extinction.infect(population) if random.random() > .99 else population
+    generation += 1
