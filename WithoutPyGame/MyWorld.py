14e90cdf251c353bdb1a56c8971497a666e83b1c
Alejandro Rosales <alejandrorosales479@gmail.com>
2019-09-23 00:47:07 -0400

    Now you can save simulations and load them later

--- WithoutPyGame/MyWorld.py
+++ WithoutPyGame/MyWorld.py
@@ -2,16 +2,25 @@
 CONVERT TO BLENDER OBJECTS - Change to "CATD" type dna or numbers or number representing "CATD"
 """
 
+import numpy as np
 import NextGen as ng
 from NextGen import Stats, MassEffect, Utils
 
-pop_n = [200, 200, 200]  # Number of: Charlens, Gritiss, Drackonians
+pop_n = [3333, 3333, 3333]  # Number of: Charlens, Gritiss, Drackonians
+
+load_file = True
+auto_save = True
+save_every = 100
+file_in = open(input("File name to load previous simulation from: ") + ".txt", "r") if load_file else None
+if load_file:
+    file_in = file_in.readlines()
+file_out = input("File name to auto save as: ") + ".txt" if auto_save else None
 
 stats = Stats()
 mass_effect = MassEffect()
 utils = Utils()
-print_every = 2
-print_plots_every = 50
+print_every = 10
+print_plots_every = 1500
 num_data_points = 5
 prob_illness = .0001
 prob_war = .0001
@@ -23,14 +32,39 @@ to_fight_species_war = 0.8
 to_fight_civil_war = 0.8
 pause = False
 pause_for_plot = True
+pause_for_auto_save = True
 save_plot = True
 
-weights = utils.create_weights()
-stats.weights_summary(weights, pause)
+if load_file:
+    weights = [float(weight) for weight in file_in.pop(0).split(",")]
+    generation = int(file_in.pop(0))
+    trait_list = ['Species', 'Height', 'Weight', 'Speed', 'Power', 'Neural Net W1', 'Neural Net W2']
+    population = []
+    creature = []
+    count = 0
+    while file_in:
+        if count % (len(trait_list)) == 0 and count != 0:
+            population.append(creature)
+            creature = []
+            count = 0
+        if trait_list[count] == 'Species':
+            creature.append((trait_list[count], file_in.pop(0).strip("\n")))
+        elif trait_list[count] == 'Neural Net W1' or trait_list[count] == 'Neural Net W2':
+            trait = ""
+            for i in range(2):
+                trait += file_in.pop(0).strip("\n").strip(" ")
+            creature.append((trait_list[count], np.array([float(t) for t in trait.split(",")])))
+        else:
+            creature.append((trait_list[count], float(file_in.pop(0).strip("\n"))))
+        count += 1
+else:
+    weights = utils.create_weights()
+    generation = 0
+    population = ng.generate_population(pop_n)
 
-generation = 0
-population = ng.generate_population(pop_n)
+stats.weights_summary(weights, pause)
 while True:
+
     # for i in range(10000):
     utils.check_pulse(population)
     stats.counting(population, generation, print_every=print_every)
@@ -45,3 +79,19 @@ while True:
     population = mass_effect.civil_war(population, prob_civil_war, weights, to_fight_civil_war, pause=pause)
 
     generation += 1
+
+    if auto_save and generation % save_every == 0:
+        def clean_text(text):
+            return text.replace("(", "").replace(")", "").replace("'", "").replace("array", "").replace("[", "").replace("]", "")
+
+        print("\nAuto saving...")
+        with open(file_out, 'w') as f:
+            f.write(clean_text(str(weights)) + "\n")
+            f.write(str(generation) + "\n")
+            for creature in population:
+                for trait in creature:
+                    cleaned_trait = clean_text(str(trait))
+                    f.write(cleaned_trait[cleaned_trait.index(",") + 2:] + "\n")
+        print("Done...")
+        if pause_for_auto_save:
+            input("\nPress ENTER to continue...")
